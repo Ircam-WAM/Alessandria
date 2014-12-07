7b5db034409a79d98f40b5d3753eb3cd486de4a9
Marc SCHNEIDER <marc@mirelsol.org>
2014-12-07 18:24:00 +0100

    Add link to Django's admin

--- bibli/urls.py
+++ bibli/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^alexandrie/admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^alexandrie/', include('alexandrie.urls', namespace='alexandrie')),
 )
