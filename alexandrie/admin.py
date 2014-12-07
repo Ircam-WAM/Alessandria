17c52383e41fecd733fa812bb19f2ff50ada7228
Marc SCHNEIDER <marc@mirelsol.org>
2014-12-07 18:29:38 +0100

    Cleanup admin part

--- alexandrie/admin.py
+++ alexandrie/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from alexandrie.models import *
 
+
 class LanguageAdmin(admin.ModelAdmin):
     list_display = ('is_default', 'label')
-
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'get_nb_copy')
 
 admin.site.register(GeneralConfiguration)
 admin.site.register(Language, LanguageAdmin)
@@ -14,10 +12,4 @@ admin.site.register(BookCondition)
 admin.site.register(BookCategory)
 admin.site.register(BookSubCategory)
 admin.site.register(BookAudience)
-
-admin.site.register(Reader)
-admin.site.register(Book, BookAdmin)
-admin.site.register(BookCopy)
-admin.site.register(Author)
-admin.site.register(Publisher)
 admin.site.register(BookTag)
