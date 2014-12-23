d1ac9d5cef52b6bd1bd9667f30335b47b60bc50e
Marc SCHNEIDER <marc@mirelsol.org>
2014-12-23 23:07:42 +0100

    Add edition name to book

--- alexandrie/models.py
+++ alexandrie/models.py
@@ -250,6 +250,7 @@ class Book(ModelEntity):
     authors = models.ManyToManyField(Author, verbose_name=u'Auteurs')
     publishers = models.ManyToManyField(Publisher, verbose_name=u'Editeurs')
     publish_date = models.DateField(u"Date de publication", null=True, blank=True)
+    edition_name = models.CharField(u"Edition", max_length=80, null=True, blank=True)
     classif_mark = models.CharField(u"Cote", max_length=10)
     height = models.PositiveIntegerField(u"Hauteur (mm)", max_length=3)
     isbn_nb = models.CharField(u"No. ISBN", max_length=30, null=True, blank=True, unique=True)
