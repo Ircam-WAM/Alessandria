441f0f3c2a0a2ab1020cbe6d8558f55f3ea4b9fb
Marc SCHNEIDER <marc@mirelsol.org>
2014-12-28 11:11:34 +0100

    Remove personal script files

--- dump_data.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-PYTHON_BIN=python
-
-#$PYTHON_BIN ./manage.py dumpdata auth.User --indent 4 > alexandrie/fixtures/users.json
-$PYTHON_BIN ./manage.py dumpdata alexandrie auth --indent 4 > alexandrie/fixtures/ref_data.json
--- reset_db.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-PYTHON_BIN=python
-DB_PATH=db/bibli.sqlite3
-rm -f $DB_PATH
-rm -rf alexandrie/migrations
-$PYTHON_BIN ./manage.py migrate && \
-$PYTHON_BIN ./manage.py loaddata --app alexandrie ref_data
-$PYTHON_BIN ./manage.py runserver
