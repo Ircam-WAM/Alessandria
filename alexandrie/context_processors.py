5079e731df56188534aefe1959a4cf643ce3f273
Marc SCHNEIDER <marc@mirelsol.org>
2015-01-19 15:12:31 +0100

    Remove stupid comment

--- alexandrie/context_processors.py
+++ alexandrie/context_processors.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from django.conf import settings # import the settings file
+from django.conf import settings
 
 def js_datepicker_lang(request):
     return {'JS_DATEPICKER_LANG': settings.LANGUAGE_CODE[:2]}
